f8c4e35373a4338e3ff2c5f83b84fc89eacee404
Wesche D <28940651+ScrapingExpert007@users.noreply.github.com>
2018-02-23 12:58:30 -0800

    3rd Commit

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from selenium.webdriver.chrome.options import Options
 
 def scraping(first_name="", last_name="", driver=None):
 
-    url = "https://extranet.cst.ucf.edu/phonebook/"
+    url = "https://directory.ufl.edu/search/?f={}&l={}&e=&a=staff".format(first_name, last_name)
     if driver is None:
         chrome_options = Options()
         chrome_options.add_argument("--headless")
@@ -30,30 +30,28 @@ def scraping(first_name="", last_name="", driver=None):
         driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='WebDriver/chromedriver.exe')
         driver.maximize_window()
 
-    driver.get(url)
-
-    # last_name= driver.find_element_by_css_selector("input#Text1")
-    last_name_entry = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "input#ctl00_ctl00_ContentPlaceHolder2_ContentPlaceHolder1_txtLastName"))
-
-    )
-    first_name_entry= driver.find_element_by_css_selector("input#ctl00_ctl00_ContentPlaceHolder2_ContentPlaceHolder1_txtFirstName")
-    submit_btn = driver.find_element_by_css_selector("input#ctl00_ctl00_ContentPlaceHolder2_ContentPlaceHolder1_btnIndividualSearch")
-
-    last_name_entry.send_keys(last_name)
-    first_name_entry.send_keys(first_name)
-    submit_btn.click()
-
     try:
-        rows = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table.individual_result_container > tbody > tr"))
-
-        )
-        phone_num = rows[0].find_elements_by_css_selector("td")[2].text.strip().replace("Phone: ", "")
-        email = rows[0].find_elements_by_css_selector("td")[1].text.strip()
-
+        driver.get(url)
     except:
         phone_num = ""
+        email = ""
+        return [phone_num, email]
+
+    try:
+        phone_num = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "p.result-phone"))
+
+        )
+        phone_num = phone_num.text
+    except:
+        phone_num = ""
+
+    try:
+        email = driver.find_element_by_css_selector("p.result-email > span")
+        email = email.text
+
+    except:
         email = ""
 
     print(phone_num)
@@ -63,5 +61,5 @@ def scraping(first_name="", last_name="", driver=None):
 
     return [phone_num, email]
 
-scraping(first_name="SELMA", last_name="ABDUL")
+scraping(first_name="ALLENE", last_name="AARON")
 # scraping(first_name="ANTHONY", last_name="ABBATE")
